ef054c69424a848d58e4bddbad2d731d76b45093
Jojo <johannes.o.andersson@gmail.com>
2016-03-03 11:17:01 +0100

    added send_job

--- cchat.erl
+++ cchat.erl
@@ -1,6 +1,6 @@
 % Top level module
 -module(cchat).
--export([server/0,client/0,start/0,start2/0]).
+-export([server/0,client/0,start/0,start2/0,send_job/3]).
 -include_lib("./defs.hrl").
 
 %% Start a server
@@ -22,3 +22,32 @@ start2() ->
     server(),
     client(),
     client().
+
+send_job(Server, F, Tasks) ->
+    try genserver:request(list_to_atom(Server), get_clients) of
+        {ok, Clients} -> 
+            Assignments = assign_tasks(Clients, Tasks),
+            Result = pmap(fun( {Client, X}) -> 
+                                  genserver:request(Client, {eval, F, X}) end, 
+                          Assignments);
+        Unkown -> io:fwrite("Unkown");
+    catch _Exception:_Reason ->
+        server_not_reached
+    end.        
+
+assign_tasks([], _) -> [] ;
+assign_tasks(Clients, Tasks) ->        
+    [{lists:nth(((N-1) rem length(Clients)) + 1, Users), Task}
+    || {N,Task} <- lists:zip(lists:seq(1,length(Tasks)), Tasks)].
+
+pmap(F, Xs) ->
+    S = self(),
+    Ref = make_ref(),
+    Pids = lists:map(fun(X) -> spawn( fun() -> S!{self(), Ref, F(X)} end)
+                     end, Xs),
+    gather(Pids, Ref).
+
+gather([Pid|T], Ref) ->
+   receive {Pid, Ref, Ret} -> [Ret|gather(T,Ref)]
+   end; 
+gather([], _) -> [].
